373b3f5f1622ede041a21149a60f4592dd476197
Luca Grazia <lucagrazia15@gmail.com>
2025-05-06 12:43:36 +0200

    Update 01_monit.R

--- 01_monit.R
+++ 01_monit.R
@@ -10,7 +10,14 @@ setwd("~/Desktop")
 mato92=rast("matogrosso_l5_1992219_lrg.jpg")
 plot(mato92)
 
+#morto galileo e nato newton 1642 che si ovvupa di onde elettromagnetiche 
 
+#bande     names: Matogrosso~2219_lrg_1, Matogrosso~2219_lrg_2, Matogrosso~2219_lrg_3 
+# banda 1= nir
+# banda 2= red
+# banda 3= green
 
+plotRGB(mato92, r=2, g=3, b=1) #metto le bande corrispondenti ai colori
+# montandol il nir sulla bli, tutto quello che sarà blu è vegetazione 
 
-
+plotRGB(mato92, r=2, g=1, b=3)
